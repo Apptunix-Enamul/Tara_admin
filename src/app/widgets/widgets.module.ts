6b4b2148f3935b4d906791fd909ba313a3eb2a0e
Enamul Haque <enamul@apptunix.com>
2021-08-31 11:29:35 +0530

    all done

--- src/app/widgets/widgets.module.ts
+++ src/app/widgets/widgets.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
-import { WidgetRoutes } from './widgets.routing';
-import { WidgetappComponent } from './apps/widget-app.component';
-import { WidgetdataComponent } from './data/widget-data.component';
+// import { WidgetRoutes } from './widgets.routing';
+// import { WidgetappComponent } from './apps/widget-app.component';
+// import { WidgetdataComponent } from './data/widget-data.component';
 
-@NgModule({
-  imports: [RouterModule.forChild(WidgetRoutes), PerfectScrollbarModule],
-  declarations: [WidgetappComponent, WidgetdataComponent]
-})
-export class WidgetsModule {}
+// @NgModule({
+//   imports: [RouterModule.forChild(WidgetRoutes), PerfectScrollbarModule],
+//   declarations: [WidgetappComponent, WidgetdataComponent]
+// })
+// export class WidgetsModule {}
