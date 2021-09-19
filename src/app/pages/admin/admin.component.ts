cba3fa7af4176c34800514718e924781665e2db5
Enamul Haque <enamul@apptunix.com>
2021-09-19 16:27:12 +0530

    sub admin

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal, ModalDismissReasons, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommonService } from 'src/app/_services/common.service';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  SubAdminData: any;
+  count: any;
 
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal,private service:CommonService) {}
 
   ngOnInit(): void {
+    this.GetSubAdmin()
   }
 // This is for the first modal
 openWindowCustomClass(content3) {
@@ -41,5 +45,84 @@ private getDismissReason(reason: any): string {
   } else {
     return  `with: ${reason}`;
   }
+}
+GetSubAdmin(){
+  let obj = {
+    "draw": 2,
+    "is_approved":true,
+    "is_active":false,
+    "columns": [
+        {
+            "data": "first_name",
+            "name": "",
+            "searchable": true,
+            "orderable": true,
+            "search": {
+                "value": "",
+                "regex": false
+            }
+        },
+        {
+            "data": "last_name",
+            "name": "",
+            "searchable": true,
+            "orderable": true,
+            "search": {
+                "value": "",
+                "regex": false
+            }
+        },
+        {
+            "data": "phone_number",
+            "name": "",
+            "searchable": true,
+            "orderable": true,
+            "search": {
+                "value": "",
+                "regex": false
+            }
+        },
+        {
+            "data": "email",
+            "name": "",
+            "searchable": true,
+            "orderable": true,
+            "search": {
+                "value": "",
+                "regex": false
+            }
+        },
+        {
+            "data": "id",
+            "name": "",
+            "searchable": true,
+            "orderable": true,
+            "search": {
+                "value": "",
+                "regex": false
+            }
+        }
+    ],
+    "order": [
+        {
+            "column": 3,
+            "dir": "undefined"        }
+    ],
+    "start": 0,
+    "length": 10,
+    "search": {
+        "value": "",
+        "regex": false
+    }
+}
+
+  this.service.get(`sub-admin/get-all-subadmin-module/`).subscribe((res:any)=>{
+  if([200,201].includes(res.code)){
+    console.log('Get subAdmin data',res);
+    this.SubAdminData = res?.data
+    this.count = res?.recordsTotal
+    
+}
+  })
 }
 }
